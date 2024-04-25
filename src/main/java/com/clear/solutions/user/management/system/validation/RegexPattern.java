618d376354d5465af8ac48492bf8a3e1ae4dba72
borsuk <b1rsu_k@protonmail.ch>
2024-04-25 17:17:02 -0400

    Separate regex pattern and error message

--- src/main/java/com/clear/solutions/user/management/system/validation/RegexPattern.java
+++ src/main/java/com/clear/solutions/user/management/system/validation/RegexPattern.java
@@ -5,11 +5,24 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class RegexPattern {
 
-    public static final String EMAIL_REGEX_PATTERN = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+    @UtilityClass
+    public class EMAIL {
+        public static final String EMAIL_REGEX_PATTERN = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+        public static final String INVALID_EMAIL_MESSAGE = "Invalid email address";
+    }
 
-    public static final String NAME_REGEX_PATTERN = "^([a-zA-Z]{2,}\\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\\s?([a-zA-Z]{1,})?)";
+    @UtilityClass
+    public class NAME {
+        public static final String NAME_REGEX_PATTERN = "^([a-zA-Z]{2,}\\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\\s?([a-zA-Z]{1,})?)";
+        public static final String INVALID_NAME_MESSAGE = "Invalid name format";
+    }
 
-    public static final String PHONE_NUMBER_REGEX_PATTERN = "^$|\\\\+(?:[0-9] ?){6,14}[0-9]$";
+    @UtilityClass
+    public class PHONE {
+        public static final String PHONE_NUMBER_REGEX_PATTERN = "^$|\\\\+(?:[0-9] ?){6,14}[0-9]$";
+        public static final String INVALID_PHONE_MESSAGE = "Invalid phone number";
+    }
+
 
 }
 
