426b2a3bc9852a5a439059646a6abb6873a8a38b
borsuk <b1rsu_k@protonmail.ch>
2024-04-25 17:18:20 -0400

    Added transaction handling to service

--- src/main/java/com/clear/solutions/user/management/system/service/UserService.java
+++ src/main/java/com/clear/solutions/user/management/system/service/UserService.java
@@ -4,12 +4,14 @@ import com.clear.solutions.user.management.system.model.User;
 import com.clear.solutions.user.management.system.repository.UserRepository;
 import com.clear.solutions.user.management.system.service.exception.AgeValidateException;
 import com.clear.solutions.user.management.system.utils.DateUtils;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
