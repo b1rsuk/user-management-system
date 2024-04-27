fe73a3ffe8198d9f24c80878993aea5d5a841f66
borsuk <b1rsu_k@protonmail.ch>
2024-04-27 10:32:01 -0400

    Added UserController, UpdateUserDto

---
+++ src/main/java/com/clear/solutions/user/management/system/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.clear.solutions.user.management.system.controller;
+
+
+import com.clear.solutions.user.management.system.model.User;
+import com.clear.solutions.user.management.system.model.dto.UpdateUserDto;
+import com.clear.solutions.user.management.system.model.dto.UserDto;
+import com.clear.solutions.user.management.system.service.UserService;
+import com.clear.solutions.user.management.system.service.filter.BirthDateRangeFilter;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public UserDto createUser(@RequestBody @Valid UserDto userDto) {
+        User createdUser = userService.save(userDto);
+
+        return createdUser.convertToDto();
+    }
+
+    @PutMapping("/{id}")
+    public UserDto updateUser(@PathVariable Long id, @RequestBody @Valid UpdateUserDto updateUserDto) {
+        User createdUser = userService.update(id, updateUserDto);
+
+        return createdUser.convertToDto();
+    }
+
+    @GetMapping
+    public List<UserDto> findAllUser(@RequestParam(value = "from", defaultValue = "1970-01-01")
+                                     @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate from,
+                                     @RequestParam(value = "to", defaultValue = "9999-12-31")
+                                     @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate to) {
+        BirthDateRangeFilter birthDateRangeFilter = new BirthDateRangeFilter(from, to);
+        return userService.findAllByBirthDateBetween(birthDateRangeFilter);
+    }
+
+}
---
+++ src/main/java/com/clear/solutions/user/management/system/model/dto/UpdateUserDto.java
@@ -0,0 +1,55 @@
+package com.clear.solutions.user.management.system.model.dto;
+
+import com.clear.solutions.user.management.system.model.BaseUser;
+import com.clear.solutions.user.management.system.validation.RegexPattern;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Validated
+public class UpdateUserDto implements BaseUser {
+
+    Long id;
+
+    @Email(regexp = RegexPattern.EMAIL.EMAIL_REGEX_PATTERN,
+            message = RegexPattern.EMAIL.INVALID_EMAIL_MESSAGE)
+    String email;
+
+    @Size(min = 2, max = 100)
+    @Pattern(regexp = RegexPattern.NAME.NAME_REGEX_PATTERN,
+            message = RegexPattern.NAME.INVALID_NAME_MESSAGE)
+    String firstName;
+
+    @Size(min = 2, max = 100)
+    @Pattern(regexp = RegexPattern.NAME.NAME_REGEX_PATTERN,
+            message = RegexPattern.NAME.INVALID_NAME_MESSAGE)
+    String lastName;
+
+    @Temporal(TemporalType.DATE)
+    @Past(message = "Value must be earlier than current date")
+    Date birthDate;
+
+    @Size(min = 5, max = 86)
+    String address;
+
+    @Size(min = 6, max = 14)
+    @Pattern(regexp = RegexPattern.PHONE.PHONE_NUMBER_REGEX_PATTERN,
+            message = RegexPattern.PHONE.INVALID_PHONE_MESSAGE)
+    String phoneNumber;
+
+}
