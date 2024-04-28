31338271f3a6a1c8b2813b84bce5a62dafcaecf4
borsuk <b1rsu_k@protonmail.ch>
2024-04-28 09:01:35 -0400

    Added deleteMapping

--- src/main/java/com/clear/solutions/user/management/system/controller/UserController.java
+++ src/main/java/com/clear/solutions/user/management/system/controller/UserController.java
@@ -9,6 +9,7 @@ import com.clear.solutions.user.management.system.service.filter.BirthDateRangeF
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,11 @@ public class UserController {
                                      @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate to) {
         BirthDateRangeFilter birthDateRangeFilter = new BirthDateRangeFilter(from, to);
         return userService.findAllByBirthDateBetween(birthDateRangeFilter);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        userService.deleteById(id);
     }
 
 }
